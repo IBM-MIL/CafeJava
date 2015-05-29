06c2e9133518bdb54c680537b83de595be45c804
John Petitto <johnpetitto@us.ibm.com>
2015-05-29 18:41:57 -0500

    Create Builder for CafeJava

--- cafejava/src/main/java/com/ibm/mil/cafejava/CafeJava.java
+++ cafejava/src/main/java/com/ibm/mil/cafejava/CafeJava.java
@@ -5,5 +5,54 @@
 
 package com.ibm.mil.cafejava;
 
-public class CafeJava {
+public final class CafeJava {
+    private final String adapterName;
+    private final String procedureName;
+    private final Object[] parameters;
+    private final int timeout;
+    private final Object invocationContext;
+
+    public static final class Builder {
+        private final static int DEFAULT_TIMEOUT = 30_000;
+
+        private final String adapterName;
+        private final String procedureName;
+
+        private Object[] parameters = new Object[] {};
+        private int timeout = DEFAULT_TIMEOUT;
+        private Object invocationContext;
+
+        public Builder(String adapterName, String procedureName) {
+            this.adapterName = adapterName;
+            this.procedureName = procedureName;
+        }
+
+        public Builder setParameters(Object... parameters) {
+            this.parameters = parameters;
+            return this;
+        }
+
+        public Builder setTimeout(int timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public Builder setInvocationContext(Object invocationContext) {
+            this.invocationContext = invocationContext;
+            return this;
+        }
+
+        public CafeJava build() {
+            return new CafeJava(this);
+        }
+    }
+
+    private CafeJava(Builder builder) {
+        adapterName = builder.adapterName;
+        procedureName = builder.procedureName;
+        parameters = builder.parameters;
+        timeout = builder.timeout;
+        invocationContext = builder.invocationContext;
+    }
+
 }
