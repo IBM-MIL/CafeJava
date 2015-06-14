ab84d7cb53f0189263c26c9d806992dcbe8344bc
John Petitto <johnpetitto@us.ibm.com>
2015-06-14 15:11:22 -0500

    Provide basic implementation for invoking JavaScript procedures

---
+++ cafejava/src/main/java/com/ibm/mil/cafejava/JSProcedureInvoker.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed Materials - Property of IBM
+ * © Copyright IBM Corporation 2015. All Rights Reserved.
+ */
+
+package com.ibm.mil.cafejava;
+
+import com.worklight.wlclient.api.WLClient;
+import com.worklight.wlclient.api.WLProcedureInvocationData;
+import com.worklight.wlclient.api.WLResponseListener;
+
+public final class JSProcedureInvoker implements ProcedureInvoker {
+    private final String adapterName;
+    private final String procedureName;
+    private Object[] parameters;
+
+    private JSProcedureInvoker(String adapterName, String procedureName) {
+        this.adapterName = adapterName;
+        this.procedureName = procedureName;
+    }
+
+    @Override
+    public void invoke(WLResponseListener wlResponseListener) {
+        // TODO: request options and compressed response
+        WLProcedureInvocationData invocationData = new WLProcedureInvocationData(adapterName,
+                procedureName);
+        invocationData.setParameters(parameters);
+        WLClient.getInstance().invokeProcedure(invocationData, wlResponseListener);
+    }
+
+    public class Builder {
+        private final String adapterName;
+        private final String procedureName;
+        private Object[] parameters;
+
+        public Builder(String adapterName, String procedureName) {
+            this.adapterName = adapterName;
+            this.procedureName = procedureName;
+        }
+
+        public Builder parameters(Object... parameters) {
+            this.parameters = parameters;
+            return this;
+        }
+
+        public JSProcedureInvoker build() {
+            JSProcedureInvoker invoker = new JSProcedureInvoker(adapterName, procedureName);
+            invoker.parameters = parameters;
+            return invoker;
+        }
+    }
+
+}
