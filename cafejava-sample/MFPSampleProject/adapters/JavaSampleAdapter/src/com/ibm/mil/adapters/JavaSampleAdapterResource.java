805381958cdfec4f5559bfd5deb08fef627130bc
Tanner Preiss <tanner.preiss@yahoo.com>
2015-06-17 16:55:32 -0500

    Created a JavaSampleAdapter which has methods for PUT, POST, GET, DELETE a user from the SampleDatabase.

--- cafejava-sample/MFPSampleProject/adapters/JavaSampleAdapter/src/com/ibm/mil/adapters/JavaSampleAdapterResource.java
+++ cafejava-sample/MFPSampleProject/adapters/JavaSampleAdapter/src/com/ibm/mil/adapters/JavaSampleAdapterResource.java
@@ -3,103 +3,126 @@
  *    5725-I43 (C) Copyright IBM Corp. 2015. All Rights Reserved.
  *    US Government Users Restricted Rights - Use, duplication or
  *    disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-*/
+ */
 
 package com.ibm.mil.adapters;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.worklight.adapters.rest.api.MFPServerOperationException;
+import com.ibm.mil.database.SampleDatabase;
+import com.ibm.mil.database.models.User;
 import com.worklight.adapters.rest.api.WLServerAPI;
 import com.worklight.adapters.rest.api.WLServerAPIProvider;
 
 @Path("/users")
 public class JavaSampleAdapterResource {
 	/*
-	 * For more info on JAX-RS see https://jsr311.java.net/nonav/releases/1.1/index.html
+	 * For more info on JAX-RS see
+	 * https://jsr311.java.net/nonav/releases/1.1/index.html
 	 */
+	public JavaSampleAdapterResource() {
 		
-	//Define logger (Standard java.util.Logger)
-	static Logger logger = Logger.getLogger(JavaSampleAdapterResource.class.getName());
-
-    //Define the server api to be able to perform server operations
-    WLServerAPI api = WLServerAPIProvider.getWLServerAPI();
-
-	/* Path for method: "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users" */
-	@GET
-	@Produces("application/xml")
-	public String hello(){
-		//log message to server log
-        logger.info("Logging info message...");
-        
-		return "<title>Hello from the Java REST adapter</title><body>Hello from the Java REST adapter</body>";
 	}
-		
-	/* Path for method: "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/{username}" */
+	
+	// Define logger (Standard java.util.Logger)
+	static Logger logger = Logger.getLogger(JavaSampleAdapterResource.class
+			.getName());
+
+	// Define the server api to be able to perform server operations
+	WLServerAPI api = WLServerAPIProvider.getWLServerAPI();
+
+	/*
+	 * Path for method:
+	 * "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/{username}"
+	 */
 	@GET
 	@Path("/{username}")
-	public String helloUser(@PathParam("username") String name){
-		return "Hello " + name;
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUser(@PathParam("username") String username) {
+		User returnedUser = SampleDatabase.getSingleton().getUser(username);
+
+		if (returnedUser == null) {
+			return Response.serverError()
+					.entity("GET FAILED: Could not find user matching given username")
+					.build();
+		}
+
+		return Response.ok().entity(returnedUser).build();
+
 	}
-	
-	/* Path for method: "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/helloUserQuery?name=value" */
-	@GET
-	@Path("/helloUserQuery")
-	public String helloUserQuery(@QueryParam("username") String name){
-		return "Hello " + name;
-	}
-	
-	/* Path for method: "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/prop" */
-	@GET
-	@Path("/prop")
-	public Response getPropertyValue() throws MFPServerOperationException{
+
+	/*
+	 * Path for method:
+	 * "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/{username}"
+	 */
+	@DELETE
+	@Path("/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteUser(@PathParam("username") String username) {
+		User deletedUser = SampleDatabase.getSingleton().deleteUser(username);
 		
-		//Get the value of the JNDI configuration property wl.analytics.url
-		String analyticsURL = api.getConfigurationAPI().getMFPConfigurationProperty("wl.analytics.url");
-		
-		return Response.ok("Analytics URL is: "+analyticsURL).build();
-	}
-	
-	/* Path for method: "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/{first}/{middle}/{last}?age=value" */
-	@POST
-	@Path("/{first}/{middle}/{last}")
-	public String enterInfo(@PathParam("first") String first, @PathParam("middle") String middle, @PathParam("last") String last,
-			@QueryParam("age") int age, @FormParam("height") String height, @HeaderParam("Date") String date){
-		return first +" "+ middle + " " + last + "\n" +
-				"Age: " + age + "\n" +
-				"Height: " + height + "\n" +
-				"Date: " + date;
-	}
-	
-	/* Path for method: "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/newUsers" */
-	@PUT
-	@Path("/newUsers")
-	public String newUsers(@FormParam("username") List<String> users){
-		if(users!=null && users.size() != 0){
-			String usersTemp = "";
-			int index = 0;
-			for (String user : users) {
-				usersTemp += " " +user;
-				if(index < users.size() - 1 && users.size() != 2) usersTemp += ",";
-				if(++index == users.size() -1 && users.size() != 1) usersTemp += " and";
-			}
-			return "Hello" + usersTemp;
+		if (deletedUser == null) {
+			return Response.serverError()
+					.entity("DELETE FAILED: Could not find user matching given username")
+					.build();
 		}
 		
-		return "Hello";
+		return Response.ok().entity(deletedUser).build();
+	}
+	
+	/*
+	 * Path for method:
+	 * "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/{username}"
+	 */
+	@PUT
+	@Path("/{username}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateUser(@PathParam("username") String username, User newUser) {
+		User updatedUser = SampleDatabase.getSingleton().updateUser(username, newUser);
+		
+		if (updatedUser == null) {
+			return Response.serverError()
+					.entity("PUT FAILED: Error updating previous user" + username)
+					.build();
+		}
+		
+		return Response.ok().entity(updatedUser).build();
+	}
+	
+	/*
+	 * Path for method:
+	 * "<server address>/MFPSampleProject/adapters/JavaSampleAdapter/users/"
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createUser(User newUser) {
+		User createdUser = SampleDatabase.getSingleton().createUser(newUser);
+		
+		if (createdUser == null) {
+			return Response.serverError()
+					.entity("POST FAILED: Error creating new user")
+					.build();
+		}
+		
+		return Response.ok().entity(createdUser).build();
+	}
+
+
+	public static void main(String[] args) {
+		JavaSampleAdapterResource test = new JavaSampleAdapterResource();
+		System.out.println(test.getUser("bobcat"));
 	}
 }
