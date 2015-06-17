4ee33d84147918ce64a028ecef3c22592d61a96b
Tanner Preiss <tanner.preiss@yahoo.com>
2015-06-17 17:13:01 -0500

    Changed SampleAdapter to JSSampleAdapter so we can distinguish between JS and java adapters.

--- cafejava-sample/MFPSampleProject/adapters/JSSampleAdapter/JSSampleAdapter-impl.js
+++ cafejava-sample/MFPSampleProject/adapters/JSSampleAdapter/JSSampleAdapter-impl.js
@@ -5,30 +5,6 @@
  *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
-/**
- *  WL.Server.invokeHttp(parameters) accepts the following json object as an argument:
- *  
- *  {
- *  	// Mandatory 
- *  	method : 'get' , 'post', 'delete' , 'put' or 'head' 
- *  	path: value,
- *  	
- *  	// Optional 
- *  	returnedContentType: any known mime-type or one of "json", "css", "csv", "plain", "xml", "html"  
- *  	returnedContentEncoding : 'encoding', 
- *  	parameters: {name1: value1, ... }, 
- *  	headers: {name1: value1, ... }, 
- *  	cookies: {name1: value1, ... }, 
- *  	body: { 
- *  		contentType: 'text/xml; charset=utf-8' or similar value, 
- *  		content: stringValue 
- *  	}, 
- *  	transformation: { 
- *  		type: 'default', or 'xslFile', 
- *  		xslFile: fileName 
- *  	} 
- *  } 
- */
 
 /**
  * getPersonFlat() returns a flat Person object.
