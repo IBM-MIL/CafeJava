a5a1cadf7ce85bd008d4423ab2235928fdf91214
John Petitto <johnpetitto@us.ibm.com>
2015-09-29 17:40:20 -0500

    Make testClassSerial test pass

--- cafejava/src/test/java/com.ibm.mil.cafejava/JsonSerializationTest.java
+++ cafejava/src/test/java/com.ibm.mil.cafejava/JsonSerializationTest.java
@@ -9,61 +9,61 @@ import com.google.gson.Gson;
 import com.worklight.wlclient.api.WLResponse;
 
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
+
+import java.util.List;
 
 import rx.Observable;
-import rx.functions.Action1;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import rx.observers.TestSubscriber;
 
 public class JsonSerializationTest {
     private Observable<WLResponse> mockObservable;
 
     @Before
     public void setUp() {
+        Person person = new Person("John", 25, true);
+
+        JSONObject jsonObject = Mockito.mock(JSONObject.class);
+        Mockito.when(jsonObject.toString()).thenReturn(new Gson().toJson(person));
+
         WLResponse wlResponse = Mockito.mock(WLResponse.class);
-        Mockito.when(wlResponse.getResponseJSON())
-                .then(new Answer<JSONObject>() {
-                    @Override
-                    public JSONObject answer(InvocationOnMock invocation) throws Throwable {
-                        Person person = new Person();
-                        person.name = "John";
-                        person.age = 25;
-                        person.isDeveloper = true;
-                        return new JSONObject(new Gson().toJson(person));
-                    }
-                });
+        Mockito.when(wlResponse.getResponseJSON()).thenReturn(jsonObject);
+
         mockObservable = Observable.just(wlResponse);
     }
 
     @Test
     public void testClassSerial() {
+        TestSubscriber<Person> testSubscriber = new TestSubscriber<>();
         mockObservable
                 .compose(CafeJava.serializeTo(Person.class))
-                .subscribe(new Action1<Person>() {
-                    @Override public void call(Person person) {
-                        assertNotNull("Person is null after serialization", person);
-                        assertEquals(person.name, "John");
-                        assertEquals(person.age, 25);
-                        assertEquals(person.isDeveloper, true);
-                    }
-                }, new Action1<Throwable>() {
-                    @Override public void call(Throwable throwable) {
-                        fail("Exception thrown: " + throwable.getMessage());
-                    }
-                });
+                .subscribe(testSubscriber);
+
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertTerminalEvent();
+
+        List<Person> persons = testSubscriber.getOnNextEvents();
+        Assert.assertFalse(persons.isEmpty());
+
+        Person person = persons.get(0);
+        Assert.assertEquals(person.name, "John");
+        Assert.assertEquals(person.age, 25);
+        Assert.assertTrue(person.isDeveloper);
     }
 
     static class Person {
         String name;
         int age;
         boolean isDeveloper;
+
+        Person(String name, int age, boolean isDeveloper) {
+            this.name = name;
+            this.age = age;
+            this.isDeveloper = isDeveloper;
+        }
     }
 
 }
